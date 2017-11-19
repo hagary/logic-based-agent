4a1d046120d6c30730c8f17fdfeb479a735d9384
Hagar Yasser <hagar.y.omar@gmail.com>
2017-11-19 20:43:44 +0200

    refinements and documentation :nail_care:

--- logic-agent/logic-agent.pl
+++ logic-agent/logic-agent.pl
@@ -1,19 +1,24 @@
+%----------------------- HOW TO QUERY? -----------------------------------------
+% query_iter(S, 1, R).
+%-------------------------------------------------------------------------------
+
 :- discontiguous agent/3.
 :- discontiguous rock/3.
 :- discontiguous blank/3.
 :- discontiguous obstacle/2.
 
-:- include('initial-state').
-:- include('query').
+%--------------------------Imports----------------------------------------------
+:- include('initial-state-Sol2').
+:- include('query-Sol2').
 :- use_module(library(clpfd)).
+%-------------------------------------------------------------------------------
 
 
-obstacle(-1,-1).
-action(up).
-action(down).
-action(right).
-action(left).
+obstacle(-1,-1). % To eliminate errors in case no obstacle is present.
 
+%------------------------Helper Predicates--------------------------------------
+
+% **next_cell** works both ways to get the position of the neighbouring cell in the direction of a given action.
 next_cell(up, R, C, RNew, CNew):-
   RNew #= R - 1, CNew #= C.
 next_cell(down, R, C, RNew, CNew):-
@@ -23,25 +28,25 @@ next_cell(right, R, C, RNew, CNew):-
 next_cell(left, R, C, RNew, CNew):-
   CNew #= C - 1, RNew #= R.
 
+% **valid** checks that a given cell position is within the dimensions of the grid.
 valid(R,C):-
   dimensions(M, N),
   R #< M, C #< N,
   R #>= 0, C #>= 0.
+%-------------------------------------------------------------------------------
 
-%-----------------------------------------------------------------------------------------------------------
-%---------------------------------------ROCK--------------------------------------------------------
-%-----------------------------------------------------------------------------------------------------------
-
+%--------------------------------ROCK-------------------------------------------
 rock(R, C, result(A,S)):-
   % It was not true & sth made it true.
   \+(obstacle(R,C)),
   valid(R,C),
-  action(A),
+  % action(A),
   next_cell(A, ROld, COld, R, C),
   next_cell(A, RAgent, CAgent, ROld, COld),
   \+(rock(R,C,S)),
   rock(ROld, COld, S),
   agent(RAgent, CAgent, S).
+
 rock(R, C, result(A,S)):-
   % It was true & nth affected it.
    rock(R,C,S),
@@ -58,40 +63,53 @@ rock(R, C, result(A,S)):-
       % Rock next to another rock.
       rock(RNew, CNew, S)
     ))).
+%-------------------------------------------------------------------------------
 
-%-----------------------------------------------------------------------------------------------------------
-%---------------------------------------AGENT--------------------------------------------------------
-%-----------------------------------------------------------------------------------------------------------
+%-------------------------------AGENT-------------------------------------------
 agent(R, C, result(A,S)):-
   % It was not true & sth made it true.
-  % Next cell is neither a rock nor an obstacle.
-  action(A),
+  % action(A),
   valid(R,C),
-  \+obstacle(R,C),
-  \+rock(R,C,S),
-  next_cell(A, RAgent, CAgent, R, C),
-  agent(RAgent, CAgent, S).
-
-agent(R, C, result(A,S)):-
-  % It was not true & sth made it true.
-  % Next cell is a rock and the agent was able to move it.
-  action(A),
-  valid(R,C),
-  rock(R,C,S),
-  next_cell(A, R, C, RNext, CNext),
-  valid(RNext, CNext),
   next_cell(A, RAgent, CAgent, R, C),
   valid(RAgent, CAgent),
+  % Aspired cell is neither a rock nor an obstacle.
+  ((
+    \+obstacle(R,C),
+    \+rock(R,C,S)
+  );
+  % Aspired cell is a movable rock.
+  (
+  next_cell(A, R, C, RNext, CNext),
+  valid(RNext, CNext),
   \+obstacle(RNext, CNext),
-  \+rock(RNext, CNext, S),
+  rock(R,C,S),
+  \+rock(RNext, CNext, S)
+  )),
   agent(RAgent, CAgent, S).
 
+
+agent(R, C, result(A,S)):-
+  % It was true and nth affected it.
+  agent(R, C, S),
+  next_cell(A, R, C, RNext, CNext),
+  (
+  % Next cell is an obstacle.
+  obstacle(RNext, CNext)
+  ;
+  (
+  % Next cell is an unmovable rock.
+  rock(RNext, CNext, S),
+  next_cell(A, RNext, CNext, RNext2, CNext2),
+  (obstacle(RNext2, CNext2) ; rock(RNext2, CNext2, S))
+  )).
+%-------------------------------------------------------------------------------
 
 query_iter(S, D, R):-
   call_with_depth_limit(query(S), D, R),
   R \= depth_limit_exceeded.
 
-query_iter(S, D, R):-
-  R = depth_limit_exceeded,
+query_iter(S, D, _):-
   D1 is D + 1,
   query_iter(S, D1, _).
+
+%----------------------------------THE END--------------------------------------
--- logic-agent/src/Main.java
+++ logic-agent/src/Main.java
@@ -1,71 +1,21 @@
-import java.util.EnumSet;
-import java.util.Random;
-
 public class Main {
 	final static int gridLimit = 5; //upper bound for dimensions of the grid.
 
 	public static void main (String[]args) throws Exception{
 		/*
-		 * Uncomment the following line to input the file name of a manual grid.
+		 * Uncomment one of the following lines to create a grid from a given file.
 		 */
-		Grid grid = Grid.createGridFromFile("Sol3");
-		/*
-		 * Generate a random grid.
-		 */
-//		Grid grid = GenGrid();
-		grid.writePrologFacts();
-		grid.writePrologQuery();
-		System.out.println(grid);
-	}
-
-	/* genGrid() generates random numbers of grid dimensions and objects,
-	 * then constructs the grid.
-	 */
-	public static Grid GenGrid(){
-		/*
-		 To generate a random number between min (inclusive) and max (exclusive), use: 
-		 random.nextInt((max - min) + 1) + min;
-		 */
-
-		Random random = new Random();
-
-		/* 
-		 * Generate grid dimensions (m x n)
-		 * max = gridLimit, min = 2
-		 */
-		int m = 0; int n= 0;
-		int upper = (gridLimit - 2) + 1;
-		m = (upper > 0)? random.nextInt(upper) + 2: 2;
-		n = (upper > 0)? random.nextInt(upper) + 2: 2;
-
-		/* Reserve one place for the initial agent location on a blank cell,
-		 * and one for the teleportal.
-		 */
-		int unoccupiedCells = m*n -2;
-		/* Adjust limit for the number of rocks/pads to a max of half
-		 * of the remaining unoccupied cells each.
-		 */
-		int rocksLimit= (unoccupiedCells/2);
-		/*
-		 * Generate random number for both rocks and pads 
-		 * with a min of 0 each.
-		 */
-		int rocks = 0, pads = 0;
-		if(rocksLimit > 0)
-			rocks = pads = random.nextInt(rocksLimit + 1);
-
-		/* 1. Adjust the limit for the obstacles to the number of remaining unoccupied cells.
-		 */
-		unoccupiedCells -= (pads+rocks);
-		int obstaclesLimit = unoccupiedCells;
-		/*2. Generate a random number of obstacles with a min of 0.
-		 */
-		int obstacles = 0;
-		if(obstaclesLimit > 0)
-			obstacles= random.nextInt(obstaclesLimit + 1);
-
-		Grid grid= new Grid(m, n, rocks, pads, obstacles);
-		grid.fillGridRandomly();
-		return grid;
+		Grid grid1 = Grid.createGridFromFile("Sol1");
+		Grid grid2 = Grid.createGridFromFile("Sol2");
+		Grid grid3 = Grid.createGridFromFile("Sol3");
+		grid1.writePrologFacts();
+		grid1.writePrologQuery();
+		grid2.writePrologFacts();
+		grid2.writePrologQuery();
+		grid3.writePrologFacts();
+		grid3.writePrologQuery();
+		System.out.println(grid1);
+		System.out.println(grid2);
+		System.out.println(grid3);
 	}
 }
